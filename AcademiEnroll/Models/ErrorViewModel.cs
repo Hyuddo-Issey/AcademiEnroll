b6eda160999d74b98aa90b537f443d95f2d58ada
Hyuddo-Issey <fa348620@gmail.com>
2024-11-11 18:00:53 -0600

    Revert "Mejoras en la vista de login y creacion de navbarAgregar archivos de proyecto."
    This reverts commit 26b83c4079609ada474819b018954c31f7460496.

--- AcademiEnroll/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AcademiEnroll.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
